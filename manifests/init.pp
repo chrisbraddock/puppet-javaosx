8282b9890477bfd6e79bf6389f69a90e38736900
xavifuefer <xavifuefer@gmail.com>
2013-05-04 11:48:47 +0200

    webstorm puppet module

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,7 @@
-# This is a placeholder class.
-class template {
-  anchor { 'Hello_World': }
-}
+class webstorm (
+  $version = '6.0.1a',
+) {
+  package { 'WebStorm':
+    provider => 'appdmg',
+    source   => "http://download.jetbrains.com/python/WebStorm-${version}.dmg",
+  }
