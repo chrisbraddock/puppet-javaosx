41d278cc18fee03a72810eace9ce71c7428610a5
Chris Braddock <braddock.chris@gmail.com>
2014-10-19 18:22:54 -0400

    try mirror for download
    as downloads seem to be sucking

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,8 +1,11 @@
 class webstorm (
   $version = '9.0-RC',
+  #$server = 'download.jetbrains.com'
+  #$server = 'download-cf.jetbrains.com'
+  $server = 'download-ln.jetbrains.com'
 ) {
   package { 'WebStorm':
     provider => 'appdmg',
-    source   => "http://download.jetbrains.com/webstorm/WebStorm-$version.dmg"
+    source   => "http://$server/webstorm/WebStorm-$version.dmg"
   }
 }
