81d3b556e9a522072a2a137eee29e60947de573a
Chris Braddock <braddock.chris@gmail.com>
2014-10-19 18:52:30 -0400

    really fix the package name for Java
    and class name

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,6 @@
-class puppetJavaOsx (
+class java (
 ) {
-  package { 'java':
+  package { 'Java':
     provider => 'appdmg',
     source   => "http://support.apple.com/downloads/DL1572/en_US/JavaForOSX2014-001.dmg"
   }
