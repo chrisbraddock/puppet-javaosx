ea7da2f87bda4e040bb386a6fb2462d883606775
Chris Braddock <braddock.chris@gmail.com>
2014-10-19 19:04:23 -0400

    think this should work fine

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,6 @@
-class javaosx (
+class java (
 ) {
-  package { 'Javaosx':
+  package { 'Java':
     provider => 'appdmg',
     source   => "http://support.apple.com/downloads/DL1572/en_US/JavaForOSX2014-001.dmg"
   }
