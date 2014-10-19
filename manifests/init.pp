aaf883897de0f9d013058037616fcacd93986e16
Chris Braddock <braddock.chris@gmail.com>
2014-10-19 18:50:26 -0400

    fixed Java package name

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,6 @@
 class puppetJavaOsx (
 ) {
-  package { 'Java':
+  package { 'java':
     provider => 'appdmg',
     source   => "http://support.apple.com/downloads/DL1572/en_US/JavaForOSX2014-001.dmg"
   }
